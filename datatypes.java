7fca2179a1e7e95e8926d57ed9a287321542c6f6
snehaangeline <110152577+snehaangeline@users.noreply.github.com>
2024-12-12 13:01:33 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,26 @@
+package Day_2;
+
+public class datatypes {
+public static void main (String args[]) {
+	byte b; //space required to hold value:1 byte
+	b=100;
+	short sh=23546;//2by
+	int i=875;//4 by
+	i=10/4;
+	long ln=54675l;//8by,no followed by letter l for long
+	float ft=45.63f;//4 by
+	double d=34562;//8 by
+	boolean bol=true;
+	System.out.println("d= "+d);
+	char c='B';//ASCII A=65,B=66,Z=90
+	char z='A';
+	char y='a';
+	int j=z+10;
+	int e=y+5;
+	System.out.println("j= "+j);
+	System.out.println("e= "+e);
+	String s="Amen";
+	System.out.println("s= "+s);
+	
+}
+}
